085e185e9026bfdc59676110eebf463ec42c3cde
shanmugavel30 <121803962+shanmugavel30@users.noreply.github.com>
2023-03-07 18:39:22 +0530

    Update AdminView.java

--- TrainBooking/admin/AdminView.java
+++ TrainBooking/admin/AdminView.java
@@ -6,58 +6,59 @@ import java.util.Scanner;
 import com.TrainBooking.TrainDetails.Train;
 import com.TrainBooking.login.LoginView;
 
-public class AdminView implements ControllerToView{
-	private  ViewToController admincontroller;
-	private Scanner scan =new Scanner(System.in);
-	
+public class AdminView implements ControllerToView {
+	private ViewToController admincontroller;
+	private Scanner scan = new Scanner(System.in);
+
 	public AdminView() {
-		admincontroller=new AdminController(this);
+		admincontroller = new AdminController(this);
 	}
-	
+
 	public void adminChoices() {
 		System.out.println("\nWelcome to admin page...!");
 		System.out.println("\n1.show all trains\n2.Add train\n3.Remove train\n4.Exit");
 		System.out.println("Enter the option: ");
-		int opt=scan.nextInt();
-		
-		if(opt==1) {
+		int opt = scan.nextInt();
+
+		if (opt == 1) {
 			admincontroller.showAllTrains();
-		}
-		else if(opt==2) {
+		} else if (opt == 2) {
 			System.out.println("Enter the train Id: ");
-			int trainId=scan.nextInt();
+			int trainId = scan.nextInt();
 			System.out.println("Enter the  source: ");
-			String source=scan.next();
+			String source = scan.next();
 			System.out.println("Enter the destination: ");
-			String destination=scan.next();
+			String destination = scan.next();
 			System.out.println("Enter the date:");
-			String date=scan.next();
+			String date = scan.next();
 			System.out.println("Enter the number of tickets: ");
-			int numOfTickets=scan.nextInt();
+			int numOfTickets = scan.nextInt();
 			System.out.println("Enter the amount to tickets: ");
-			int amount=scan.nextInt();
+			int amount = scan.nextInt();
 			System.out.println("Enter the thakkal ticket amount :");
-			int thakkalamount=scan.nextInt();
-			admincontroller.addTrain(trainId,source,destination,date,numOfTickets,amount,thakkalamount);
-		}
-		else if(opt==3) {
+			int thakkalamount = scan.nextInt();
+			admincontroller.addTrain(trainId, source, destination, date, numOfTickets, amount, thakkalamount);
+		} else if (opt == 3) {
 			System.out.println("Enter the trainId to  remove: ");
-			int trainId=scan.nextInt();
+			int trainId = scan.nextInt();
 			admincontroller.removeTrain(trainId);
-			
-		}
-		else {
+
+		} else {
 			System.out.println("Returning to thr home page---#");
-			LoginView ref=new LoginView();
+			LoginView ref = new LoginView();
 			ref.loginWeb();
 		}
 	}
 
 	public void allTrainsList(List<Train> allTrains) {
-		System.out.printf("\n %10s %13s %13s %13s %13s %13s %13s %13s %13s %13s","TrainId","Source","Destination","Date","NoOfSheets","UpperSeats","MiddleSeats","LowerSeats","RAC","WaitingList");
-		for(int i=0;i<allTrains.size();i++) {
-			System.out.printf("\n %10s  %13s %13s %13s %13s %12s %12s %12s %13s %13s",allTrains.get(i).getTrainId(),allTrains.get(i).getsource(),allTrains.get(i).getdestination(),allTrains.get(i).getdate(),allTrains.get(i).getNoOfTicket(),
-					allTrains.get(i).getUpperSeats(),allTrains.get(i).getMiddleSeats(),allTrains.get(i).getLowerSeats(),allTrains.get(i).getRac(),allTrains.get(i).getWaitingList());
+		System.out.printf("\n %10s %13s %13s %13s %13s %13s %13s %13s %13s %13s", "TrainId", "Source", "Destination",
+				"Date", "NoOfSheets", "UpperSeats", "MiddleSeats", "LowerSeats", "RAC", "WaitingList");
+		for (int i = 0; i < allTrains.size(); i++) {
+			System.out.printf("\n %10s  %13s %13s %13s %13s %12s %12s %12s %13s %13s", allTrains.get(i).getTrainId(),
+					allTrains.get(i).getsource(), allTrains.get(i).getdestination(), allTrains.get(i).getdate(),
+					allTrains.get(i).getNoOfTicket(), allTrains.get(i).getUpperSeats(),
+					allTrains.get(i).getMiddleSeats(), allTrains.get(i).getLowerSeats(), allTrains.get(i).getRac(),
+					allTrains.get(i).getWaitingList());
 		}
 		adminChoices();
 	}
