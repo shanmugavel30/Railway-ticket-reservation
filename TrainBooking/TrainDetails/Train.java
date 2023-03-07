40112f9cc4f927b23d48c1658b7b55b9adf72489
shanmugavel30 <121803962+shanmugavel30@users.noreply.github.com>
2023-03-07 16:33:46 +0530

    Update Train.java

--- TrainBooking/TrainDetails/Train.java
+++ TrainBooking/TrainDetails/Train.java
@@ -34,9 +34,9 @@ public class Train {
 			this.noOfTicket=noOfTicket;
 			this.amount=amount;
 			this.thakkalAmount=thakkalAmount;
-			upperSeats=15;
-			middleSeats=15;
-			lowerSeats=15;
+			upperSeats=noOfTicket/3;
+			middleSeats=noOfTicket/3;
+			lowerSeats=noOfTicket/3;
 			rac=10;
 			waitingList=5;
 			thakkal=15;
@@ -54,9 +54,9 @@ public class Train {
 			this.setSource(Source);
 			this.setDestination(Destination);
 			this.setDate(Date); 
-			upperSeats=15;
-			middleSeats=15;
-			lowerSeats=15;
+			upperSeats=noOfTicket/3;
+			middleSeats=noOfTicket/3;
+			lowerSeats=noOfTicket/3;
 			rac=10;
 			waitingList=5;
 			lowerBerthsPositions= new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15));
